fcfb35ddc3478b24b70b40023f005f108f853087
eugeniup <eugen958.ep@gmail.com>
2024-05-27 12:00:15 +0300

    Wrapper with specific type

---
+++ src/main/java/student/examples/App.java
@@ -0,0 +1,17 @@
+package student.examples;
+
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Wrapper wi = new Wrapper(1000);
+
+        System.out.println(wi.getValue());
+    }
+}
---
+++ src/main/java/student/examples/Wrapper.java
@@ -0,0 +1,16 @@
+package student.examples;
+
+public class Wrapper {
+    private Integer value;
+    public Wrapper(Integer value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
