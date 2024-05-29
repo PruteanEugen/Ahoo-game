dd7196abce29a9e80cc061ab173686cd7b7fc638
eugeniup <eugen958.ep@gmail.com>
2024-05-29 10:56:25 +0300

    Example two fisished

--- src/main/java/student/examples/App.java
+++ src/main/java/student/examples/App.java
@@ -16,7 +16,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        Scanner scanner = new Scanner(System.in);
+      //  Scanner scanner = new Scanner(System.in);
 
 
 
@@ -31,35 +31,25 @@ public class App
         // CRUD - CREATE / READ / UPDATE/ DELETE    BREAD - BROWS /READ / EDIT / ADD / DELETE
         //Generics
         //    v
-        List<Object> users = new ArrayList<>(); //SO[L]I[D]
-
+        List<User> users = new ArrayList<>(); //SO[L]I[D]
 
         while (users.size() != 5) {
             users.add(new User(String.format("User %d" , users.size()+1)));
 
             }
-        System.out.print("Enter username: ");
-        String username = scanner.nextLine();
-        System.out.print("Enter index: ");
-        int index = scanner.nextInt();
-        ((User)users.get(index)).setUsarname(username);
 
-//        for (int i = 0; i < users.size() ; i++) {
-//            System.out.println(users.get(i));
-//
-//
-//        }
-//        for (User user : users) {
-//            System.out.println(user);
-//        }
-        ((User)users.get(2)).setUsarname("User 3 new");
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).getUsarname().equals("User 3")) {
+                users.remove(i);
+            }
+        }
+        //users.remove(new User("User 1"));
+
+
         users.forEach(System.out::println);
 
     }
 }
 
-//        users.add(new User("User 1"));
-//        users.add(new User("User 2"));
-//        users.add(new User("User 3"));
-//        System.out.println("Users:" + users.size());
+
 
